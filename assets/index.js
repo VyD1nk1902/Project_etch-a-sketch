32d229b41eb6f981d155d15650b7f4a9fa8b1de6
VyDink <vy.dinh.2929@gmail.com>
2025-04-14 14:32:31 +0700

    Add a function for user to make a new grid layout base on their input Ex: 3 will render a square 3x3 and so on
    In this function I use while(true) to make sure user input the right
    value and if they cancel the action of creating new layout, the function
    will stop. If they input wrong value, the alert will continue pop-up
    until the valid value is inputed.

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,76 @@
+const body = document.querySelector("body");
+
+const button = document.createElement("button");
+button.setAttribute("class", "grid-btn");
+body.appendChild(button);
+button.textContent = "New Grid Layout";
+
 const div = document.createElement("div");
 div.setAttribute("class", "grid-container");
-const body = document.querySelector("body");
 body.appendChild(div);
 
 const gridLayout = document.querySelector(".grid-container");
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
+  square.setAttribute("class", "grid-square");
   gridLayout.appendChild(square);
 }
+
+// tạo button khi click vào -> xóa grid hiện tại -> tạo grid mới dựa trên input người dùng
+// nhập 3 thì cho ra 3x3 , 4 thì 4x4)
+// nếu nhập sai điều kiện thì sẽ cần nhập lại đến khi nào thỏa điều kiện (dùng While(true))
+// nếu bấm cancel thì sẽ kết thúc hàm.
+
+button.addEventListener("click", () => {
+  let newLayoutInput;
+  let userInput;
+
+  while (true) {
+    userInput = prompt("What size of the square (height x width) do you want ?(1 - 99)");
+
+    if (userInput === null) {
+      alert("You Canceled.");
+      return;
+    }
+
+    newLayoutInput = Number(userInput);
+    if (Number.isInteger(newLayoutInput) && newLayoutInput > 0 && newLayoutInput <= 99) {
+      break;
+    }
+    alert("Invalid number. Please add number 1-99");
+  }
+
+  const gridInput = newLayoutInput * newLayoutInput;
+
+  function newGrid(gridInput) {
+    gridLayout.textContent = "";
+    gridLayout.style.gridTemplateColumns = `repeat(${newLayoutInput}, 1fr)`;
+    gridLayout.style.gridTemplateRows = `repeat(${newLayoutInput}, 1fr)`;
+    for (let i = 0; i < gridInput; i++) {
+      const square = document.createElement("div");
+      square.setAttribute("class", "grid-square");
+      gridLayout.appendChild(square);
+    }
+  }
+
+  newGrid(gridInput);
+});
+
+//   let newLayoutInput = Number(prompt("How many number of squares per side do you want for the new grid ?(1 - 99)"));
+//   newLayoutInput = Number(userInput);
+//   const gridInput = newLayoutInput * newLayoutInput;
+
+//   function newGrid(gridInput) {
+//     gridLayout.textContent = "";
+//     gridLayout.style.gridTemplateColumns = `repeat(${newLayoutInput}, 1fr)`;
+//     gridLayout.style.gridTemplateRows = `repeat(${newLayoutInput}, 1fr)`;
+//     for (let i = 0; i < gridInput; i++) {
+//       const square = document.createElement("div");
+//       gridLayout.appendChild(square);
+//     }
+//   }
+
+//   if (Number.isInteger(newLayoutInput) && newLayoutInput > 0 && newLayoutInput <= 99) {
+//     newGrid(gridInput);
+//   } else alert("Invalid number. Please add number 1-99");
+// });
